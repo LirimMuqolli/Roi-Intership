419e69afbb7e8928eb2d4649ec18e820c946fd69
LirimMuqolli <57811400+LirimMuqolli@users.noreply.github.com>
2022-09-02 20:26:45 +0200

    create a videoBg

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,11 +2,19 @@ import React from "react"
 import "./about.css"
 import Back from "../common/back/Back"
 import AboutCard from "./AboutCard"
-
+import videoBg from "../about/videoBg.mp4"
 const About = () => {
   return (
     <>
       <Back title='About Us' />
+      <div className='bg-image d-flex justify-content-center align-items-center'>
+        <div className="overlay"></div>
+        <video src={videoBg} autoPlay loop muted />
+        <div className="content">
+            <h1>Welcome</h1>
+            <p>To my site.</p>
+        </div>
+    </div>
       <AboutCard />
     </>
   )
