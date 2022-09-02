0f0838260846ff047601d9a66471b4558b66828a
LirimMuqolli <57811400+LirimMuqolli@users.noreply.github.com>
2022-09-02 20:53:13 +0200

    upd

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,10 +3,12 @@ import "./about.css"
 import Back from "../common/back/Back"
 import AboutCard from "./AboutCard"
 import videoBg from "../about/videoBg.mp4"
+import Heading from "../common/heading/Heading"
 const About = () => {
   return (
     <>
       <Back title='About Us' />
+      <Heading subtitle='VIDEO' title=' Of Our Company' />
       <div className=' justify-content-center align-items-center  height: 100vh' >
         <div className="justify-content-center align-items-center  height: 100vh"></div>
         <video src={videoBg} autoPlay loop muted />
